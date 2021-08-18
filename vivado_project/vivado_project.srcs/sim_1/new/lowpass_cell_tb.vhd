dc9ce4be61a4eb36d16705aafcf2506c7c0c7796
Zack <heatedphoenix@gmail.com>
2021-08-18 13:51:02 +0200

    Added working lowpass cells

---
+++ vivado_project/vivado_project.srcs/sim_1/new/lowpass_cell_tb.vhd
@@ -0,0 +1,101 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/27/2021 10:34:51 PM
+-- Design Name: 
+-- Module Name: lowpass_cell_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+library ieee_proposed;
+use ieee_proposed.fixed_pkg.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity lowpass_cell_tb is
+--  Port ( );
+end lowpass_cell_tb;
+
+architecture Behavioral of lowpass_cell_tb is
+
+    constant IMG_WIDTH      : natural := 64;
+    constant IMG_HEIGHT     : natural := 64;
+    constant NUM_BITS_PIXEL : natural := 8;    
+    constant NUM_BITS_ADDR  : natural := 8;
+    constant NUM_BITS_COEFF : natural := 16;
+
+    signal rst : std_logic := '0';
+    signal clk : std_logic := '0';
+    signal input : sfixed (2*NUM_BITS_PIXEL-1 downto -4);
+    signal output : sfixed (2*NUM_BITS_PIXEL-1 downto -4);
+    
+    signal lowpass_enable : std_logic := '0';
+
+begin
+
+uut: entity work.lowpass_cell(Behavioral) 
+    generic map (
+            NUM_BITS_PIXEL  => 8, -- 24 total = R + G + B (8-bits each)  
+            NUM_BITS_ADDR   => 8,           
+            MAX_IMG_WIDTH   => 128,
+            MAX_IMG_HEIGHT   => 128
+            )
+    Port map ( 
+           rst => rst,
+           clk => clk,
+           lowpass_enable => lowpass_enable,
+           spike_input => input,
+           filtered_output => output
+           );
+
+clk_proc: process
+begin
+    clk <= '0';
+    wait for 5 ns;
+    clk <= '1';
+    wait for 5 ns;        
+end process;
+
+process	--input process simulation only
+begin
+    wait until rising_edge(clk);
+    rst <= '1';
+    lowpass_enable <= '0';
+    wait until rising_edge(clk);
+    rst <= '0';
+    lowpass_enable <= '1';
+    input <= to_sfixed(1.5, 2*NUM_BITS_PIXEL-1, -4);
+    wait for 20 ns;
+    input <= to_sfixed(1.75, 2*NUM_BITS_PIXEL-1, -4);
+    --lowpass_enable <= '0';
+    wait for 20 ns;
+    input <= to_sfixed(0, 2*NUM_BITS_PIXEL-1, -4);
+    --lowpass_enable <= '1';
+    wait for 100 ns;
+    --lowpass_enable <= '0';
+end process;
+
+end Behavioral;
--- vivado_project/vivado_project.srcs/sources_1/new/lowpass_cell.vhd
+++ vivado_project/vivado_project.srcs/sources_1/new/lowpass_cell.vhd
@@ -43,33 +43,44 @@ use ieee_proposed.fixed_pkg.all;
 entity lowpass_cell is
    generic (
             NUM_BITS_PIXEL  : natural := 8; -- 24 total = R + G + B (8-bits each)  
+            NUM_BITS_FIXED_INT : natural := 4;
+            NUM_BITS_FIXED_FRAC : natural := 11;
             NUM_BITS_ADDR   : natural := 8;           
             MAX_IMG_WIDTH   : natural := 64;
             MAX_IMG_HEIGHT   : natural := 64
             );
     Port ( rst : in STD_LOGIC;
            clk : in std_logic;
-           spike_input : in STD_LOGIC_VECTOR (2*NUM_BITS_PIXEL-1 downto 0);
-           filtered_output : out STD_LOGIC_VECTOR (2*NUM_BITS_PIXEL-1 downto 0)
+           lowpass_enable : in std_logic;
+           spike_input : in sfixed (2*NUM_BITS_PIXEL-1 downto -4);
+           filtered_output : out sfixed (2*NUM_BITS_PIXEL-1 downto -4)
            );
 end lowpass_cell;
 
 architecture Behavioral of lowpass_cell is
 
-signal prior_output : std_logic_vector(2*NUM_BITS_PIXEL-1 downto 0);
-signal tau : std_logic_vector(2*NUM_BITS_PIXEL-1 downto 0);
+signal prior_output : sfixed(2*NUM_BITS_PIXEL-1 downto -4);
+signal tau : sfixed(2*NUM_BITS_PIXEL-1 downto -4);
+signal intermediate_output : sfixed(2*NUM_BITS_PIXEL-1 downto -4);
 
 begin
 
 process(clk, rst) is
 begin
     if rst = '1' then
+        prior_output <= (others => '0');
+        intermediate_output <= (others => '0');
+        tau <= to_sfixed(0.5, 2*NUM_BITS_PIXEL-1, -4); -- (value, high downto, low)
     end if;
     if rising_edge(clk) then
-        filtered_output <= prior_output + tau * (spike_input - prior_output);
+        if lowpass_enable = '1' then
+            intermediate_output <= resize(prior_output + tau * (spike_input - prior_output), intermediate_output'high, intermediate_output'low);
+            -- can we do something clever with shifts here? is there a range for Tau?
+            filtered_output <= intermediate_output;
+            prior_output <= intermediate_output;
+        end if;
     end if;
 end process;
-
 
 
 end Behavioral;
